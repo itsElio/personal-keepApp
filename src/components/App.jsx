e14471efb1283cedd93f62633509d6f9915a0cd4
YourFavElio <sistoelio@gmail.com>
2020-04-25 13:06:52 +0200

    fix functions to fetch notes from mongodb and display/delete them

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-
-
+import axios from "axios";
 
 //Esegue il render dei singoli componenti Header, Footer, ed itera la funzione createNote 
 // per ogni elemento all'interno di "notes", importato da notes.js
@@ -13,32 +12,59 @@ import CreateArea from "./CreateArea";
 function App(props) { 
 
     const [notes, setNotes] = useState([]);
+    const [load, setLoad] = useState(false);
 
     function addNote(newNote) {
-        console.log(newNote);
         setNotes(prevNotes => {
             // con ...prevNotes reinserisce tutti gli oggetti dell'array e ne aggiunge un altro (newNote) passato alla funzione
             return [...prevNotes, newNote];
-       });
+       }); 
     }
 
-    function deleteNote(id) {
+    //esegue una richiesta Get ogni volta che la pagina viene aggiornata, prende tutti i dati dal database, tramite funzione
+    // setNotes aggiorna lo state, aggiungendo le Note ricevute come risposta
+    function onLoad() {
+        axios.get("/api")
+        .then(function (response) {
+          setNotes(response.data);
+        });
+        return;
+    }
+    
+    // utilizzo uno state per chiamare solo una volta la funzione onLoad(), poichè, visto che il documento esegue il reload
+    // ogni volta che viene aggiunta una nuova nota, la funzione onLoad() inviava numerose richieste al server.
+    function isLoaded() {
+        if(!load) {
+            onLoad();
+            setLoad(true);
+        } else {
+            return;
+        }
+    } 
+    
+    function deleteNote(passedId) {
+        
         setNotes(prevNotes =>{
             return prevNotes.filter((noteItem, index) => {
-                return index !== id;
+                return passedId !== noteItem.id;
             });
-        })
+        }) 
+
+        axios.delete("/delete", { data: { id: passedId } });
+       //onLoad();
+        console.log(passedId);
     }
 
    return(
     <div>
         <Header />
         <CreateArea onAdd={addNote} />
+        {isLoaded()}
         {notes.map((noteItem, index) =>{
             return (
         <Note 
             key={index}
-            id={index}
+            id={noteItem.id}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
